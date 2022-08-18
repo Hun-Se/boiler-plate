1fc63dc07fedc470555019d22d57a33fd50e16a6
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-08-19 00:24:51 +0900

    비밀 정보 보호 및body-parser 추가

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const confing = require("./config/key");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 형식을 분석 할 수 있게 해준다.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 형식을 분석 할 수 있게 해준다.
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://Hun-Se:jj95929592@boiler-loginproject.0nnogr4.mongodb.net/?retryWrites=true&w=majority",
-    {
-      // useNewUrlPaser: true,
-      // useUnifiedTofology: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
-    }
-  )
+  .connect(confing.mongoURI, {
+    // useNewUrlPaser: true,
+    // useUnifiedTofology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB conected"))
   .catch((err) => {
     console.log(err);
   });
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!~~안녕하세요"));
+
+app.post("/register", (req, res) => {
+  //회원 가입 할떄 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+  //mongoDB 메서드
+  user.save((err, userInfo) => {
+    // 전달에 실패 했을때
+    if (err) return res.json({ success: false, err });
+    // status(200)은 성공했다는 뜻이다.
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
