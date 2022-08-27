9b92f3585be7b6eb6763e3ab1ecf25c8fee238d3
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-08-27 15:19:50 +0900

    redux 오류 해결

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useDispatch } from "react-redux";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    Axios.post("/api/users/login", body).then((response) => {});
+  };
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <label>Email</label>
+      <input type="email" value={Email} onChange={onEmailHandler} />
+      <label>Password</label>
+      <input type="password" value={Password} onChange={onPasswordHandler} />
+      <button>Login</button>
+    </form>
+  );
 }
 
 export default LoginPage;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 import App from "./App";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
